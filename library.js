b2b3fbb77c82f6bc8548e7c9acb1b875725c862d
stelladream <neonykim@gmail.com>
2023-11-15 12:20:56 +0900

    Update library.js

--- library.js
+++ library.js
@@ -8,7 +8,7 @@
 	var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
 
 	var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
-	var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
+	//var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
 
 	Youtube.parse = function(data, callback) {
 		if (!data || !data.postData || !data.postData.content) {
@@ -20,9 +20,9 @@
 		if (data.postData.content.match(regularUrl)) {
 			data.postData.content = data.postData.content.replace(regularUrl, embed);
 		}
-		if (data.postData.content.match(shortUrl)) {
-			data.postData.content = data.postData.content.replace(shortUrl, embed);
-		}
+		//if (data.postData.content.match(shortUrl)) {
+		//	data.postData.content = data.postData.content.replace(shortUrl, embed);
+		//}
 
 		callback(null, data);
 	};
